ca94f478528c0aad2585c7152dcd2ab547df05a4
goreRatzete <avolkmann@icloud.com>
2016-05-01 13:50:36 +0200

    init

---
+++ gulpfile.babel.js
@@ -0,0 +1,55 @@
+'use strict';
+
+require('babel-core/register');
+require('babel-polyfill');
+import gulp from 'gulp';
+import nodemon from 'gulp-nodemon';
+import ngConstant from 'gulp-ng-constant';
+const argv = require('yargs').argv;
+
+const environment = argv.env || 'development';
+const port = argv.port || 3000;
+const config = argv.config;
+
+// Default
+gulp.task('default', async() => {
+    console.log(`Environment: ${environment}`);
+    console.log(`Port: ${port}`);
+    let ip = await Promise.resolve(getIpAddress());
+    console.log(`Ip: ${ip}`);
+
+    if (config) {
+        await gulp.src('config/' + 'client' + '.json')
+            .pipe(ngConstant({
+                name: 'appConfig',
+                constants: {
+                    ip: `${ip}:${port}`
+                }
+            }))
+            .pipe(gulp.dest('public/javascripts/'));
+    }
+
+    nodemon({
+        exec: "babel-node --presets=es2015,stage-3",
+        script: 'bin/www',
+        ext: 'js',
+        env: {
+            'NODE_ENV': environment,
+            'PORT': port,
+            'IP': ip
+        }
+    });
+});
+
+
+// get the local ip address for this computer
+function getIpAddress() {
+    return new Promise((resolve, reject) => {
+        require('dns').lookup(require('os').hostname(), function (err, add, fam) {
+            if (err) reject(err);
+            else {
+                resolve(add);
+            }
+        });
+    });
+}
